f3d9cfe136ab7a942ba666087b6d61a6b447610d
Khok Shokhanul <schohan430@gmail.com>
2016-11-01 15:54:41 +0200

    2nd task solved added LCS.cs

---
+++ tasks/KhokHW/WordBreaker/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileParser d = new FileParser();
+            string[] dictionary = d.GetWords();
+            Console.WriteLine("Enter string.");
+            string str = Console.ReadLine();
+            WordDivider w = new WordDivider(str, dictionary);
+            w.WordsBrake();
+        }
+    }
+}
